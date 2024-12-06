713ae719566b491a05e931140707b35fbc5856ea
a1t3m1s <lifiumb@gmail.com>
2024-12-06 19:19:22 +0400

    commit_3

--- TeamfightTactics/Assets/Scripts/GameManager/GameManager.cs
+++ TeamfightTactics/Assets/Scripts/GameManager/GameManager.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.AI.Navigation;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
+    [Header("Nav Mesh")]
+    [field: SerializeField]
+    private NavMeshSurface surface;
+
+    [Header("Hex Grid Game Objects")]
     [field: SerializeField]
     private GameObject playerHexGrid;
+    private List<GameObject> playerUnits;
     [field: SerializeField]
     private GameObject enemyHexGrid;
+    private List<GameObject> enemyUnits;
+
+    [Header("Bundles")]
+    [field: SerializeField]
+    private GameObject planeHelder;
+    [field: SerializeField]
+    private GameObject playerBenchHelder;
+    private List<Transform> playerBench;
+    [field: SerializeField]
+    private GameObject enemyBenchHelder;
+
+    [Header("Buttons")]
+    [field: SerializeField]
+    private Button startFightBtn;
 
     private void Awake()
     {
+        startFightBtn.onClick.AddListener(StartFightOnClick);
+
         playerHexGrid.SetActive(false);
-        //enemyHexGrid.SetActive(false);
+
+        playerBench = new List<Transform>();
+        int childCounter = playerBenchHelder.transform.childCount;
+        for (int i=0; i<childCounter; ++i)
+        {
+            playerBench.Add(playerBenchHelder.transform.GetChild(i));
+        }
     }
 
     private void Update()
@@ -26,4 +60,54 @@ public class GameManager : Singleton<GameManager>
             playerHexGrid.SetActive(false);
         }
     }
+
+    #region StartFightBtn
+    private void SetActiveGO(bool isEnable)
+    {
+        planeHelder.SetActive(isEnable);
+        playerBenchHelder.SetActive(isEnable);
+        enemyBenchHelder.SetActive(isEnable);
+    }
+
+    private void GetAllUnitsFromHexGrid(ref GameObject hexGrid, ref List<GameObject> units)
+    {
+        units = new List<GameObject>();
+        int hexGridCounter = hexGrid.transform.childCount;
+        for (int i = 0; i < hexGridCounter; ++i)
+        {
+            Transform hexTileTransform = hexGrid.transform.GetChild(i);
+            if(hexTileTransform.childCount > 0)
+            {
+                units.Add(hexTileTransform.GetChild(0).gameObject);
+            }
+        }
+    }
+
+    private void SetMeshAgentComponent(ref List<GameObject> units)
+    {
+        for(int i=0; i< units.Count; ++i)
+        {
+            units[i].AddComponent<NavMeshAgent>();
+        }
+    }
+
+    private void StartFightOnClick()
+    {
+        SetActiveGO(false);
+
+        GetAllUnitsFromHexGrid(ref playerHexGrid, ref playerUnits);
+        GetAllUnitsFromHexGrid(ref enemyHexGrid, ref enemyUnits);
+
+        SetMeshAgentComponent(ref playerUnits);
+        SetMeshAgentComponent(ref enemyUnits);
+
+        surface.BuildNavMesh();
+        
+        SetActiveGO(true);
+
+
+    }
+    #endregion
+
+
 }
--- TeamfightTactics/Assets/Scripts/Objects/CharacterController.cs
+++ TeamfightTactics/Assets/Scripts/Objects/CharacterController.cs
@@ -1,14 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class CharacterController
+public class CharacterController : MonoBehaviour
 {
-    public int hitPoints; //hit points
-    public int energyPoints; //energy points
-    public float attackSpeed;
-    public float magicResist;
-    public float physicResist;
+    //public int hitPoints; //hit points
+    //public int energyPoints; //energy points
+    //public float attackSpeed;
+    //public float magicResist;
+    //public float physicResist;
 
-    protected virtual void UseUltimateSkill() { }
+    public NavMeshAgent agent;
+    public List<Transform> enemies;
+
+    public LayerMask whatIsGround;
+    public LayerMask whatIsEnemy;
+
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange;
+
+    [Header("Attacking")]
+    public float timeBetweenAttacks;
+    public bool alreadyAttack;
+
+    [Header("States")]
+    public float sightRange;
+    public float attackRange;
+    public bool enemyInSightRange;
+    public bool enemyInAttackRange;
+
+    public void SetCharacterSettings(List<GameObject> enemies, NavMeshAgent agent)
+    {
+        this.enemies = new List<Transform>();
+        foreach(var enemy in enemies)
+        {
+            this.enemies.Add(enemy.transform);
+        }
+
+        this.agent = agent;
+    }
+
+    public void UpdateStatement()
+    {
+        //enemyInSightRange = Physics.CheckSphere();
+    }
+
 }
